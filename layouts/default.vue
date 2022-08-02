33f2e6d849efecd6e25d7a30f96d7a2f4ae5b44b
Ellanski <nameimprobable@gmail.com>
2022-08-02 06:06:10 +0400

    scaffolding

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,22 @@
+<script lang="ts" setup>
+import { useQuery } from 'vue-query'
+
+const { data, refetch } = useQuery(
+  ['https://jsonplaceholder.typicode.com/posts'],
+  ({ queryKey }) => $fetch(queryKey[0]),
+  {
+
+  },
+)
+</script>
+
 <template>
   <main class="py-20 px-10 text-center">
     <slot />
     <Footer />
-    <div class="mt-5 mx-auto text-center opacity-25 text-sm">
-      [Default Layout]
-    </div>
+    <button @click="refetch()">
+      Refetch Posts
+    </button>
+    <pre>{{ data }}</pre>
   </main>
 </template>
